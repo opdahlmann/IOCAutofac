16935d88b057f07e30e365e4f239ea8c18734a07
Ole Petter Dahlmann <olepetter@dahlmann.no>
2016-12-08 08:27:45 +0100

    Add project files.

---
+++ IOCAutofac.API/App_Start/IocConfig.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using IOCAutofac.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace IOCAutofac.API
+{
+    public class IocConfig
+    {
+        public static void Configure()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<TestService>().AsSelf().InstancePerRequest();
+            var container = builder.Build();
+            var resolver = new AutofacWebApiDependencyResolver(container);
+            GlobalConfiguration.Configuration.DependencyResolver = resolver;
+        }
+    }
+}
---
+++ IOCAutofac.API/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using IOCAutofac.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace IOCAutofac.API.Controllers
+{
+    public class TestController : ApiController
+    {
+        private TestService _testService;
+
+        public TestController(TestService testService)
+        {
+            _testService = testService;
+        }
+
+        [HttpGet]
+        [Route("ping")]
+        public async Task<HttpResponseMessage> GetPing()
+        {
+            var ping = await _testService.GetPing();
+            return Request.CreateResponse(HttpStatusCode.OK, ping);
+        }
+    }
+}
